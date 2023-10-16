01e52b721b945c263d716a11e9aa72974e539835
Dave Sebastian <darksebastianwijono@gmail.com>
2023-10-16 15:34:50 +0700

    redirect to home

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect('/home');
+});
 
 Route::get('/help', function () {
     return view('welcome');
@@ -33,3 +36,7 @@ Route::get('/tracker', function () {
 Route::get('/blade_template', function () {
     return view('template\template');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
