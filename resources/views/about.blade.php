3b9807763a1620b789699cbb88c1e5da075a79c3
Nathan Setiawan <natsetiawan03@gmail.com>
2023-11-07 12:30:57 +0700

    added dummy text at about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,24 @@
 @endsection
 
 @section('container')
+<div class="container">
     <h1>About Us</h1>
+    <p>Welcome to StrollStats!</p>
+
+    <section class="mission">
+        <h3>Our Mission</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, consectetur.</p>
+    </section>
+
+    <section class="values">
+        <h3>Our Values</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, saiente?</p>
+    </section>
+
+    <section class="team">
+        <h3>Meet Our Team</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, explicabo.</p>
+    </section>
+
+</div>
 @endsection
