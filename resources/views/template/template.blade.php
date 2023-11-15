f6d894d7faba3bd48b214f18d1bb4e8a91ba8cd7
DH3155 <darian.elbert@gmail.com>
2023-11-15 12:12:00 +0700

    Started Progress

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -42,6 +42,7 @@
     <div>
         @yield('container')
     </div>
+<<<<<<< HEAD
     <a href="/home">
         <button id="plusBtn">
             +
@@ -67,6 +68,8 @@
             </div>
         </div>
     </footer>
+=======
+>>>>>>> 787a63558d6576e7892a7cc21b1181becbe42723
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
