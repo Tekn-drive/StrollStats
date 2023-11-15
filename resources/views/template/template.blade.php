2762f299a263552945ab945385d5ad484251d709
Nathan Setiawan <natsetiawan03@gmail.com>
2023-11-15 09:35:05 +0700

    baseline for footer

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -33,6 +33,16 @@
     <div>
         @yield('container')
     </div>
+
+    <footer class="justify-content-between align-items-center py-3 my-4 border-top, padding-left:0;">
+      <div class="container d-flex justify-content-between" style="padding-left: 1; margin-left: 0;">
+          <div class="col-md-4 align-items-center">
+              <a class="navbar-brand" href="{{ url('/') }}">StrollStats</a>
+              <span class="mb-3 mb-md-0 text-body-secondary">&copy; {{ date("Y") }}</span>
+          </div>
+      </div>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
