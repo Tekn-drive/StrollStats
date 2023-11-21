0dcdedadb0e870a664192709da04ac625fdd476f
DH3155 <darian.elbert@gmail.com>
2023-11-21 11:25:16 +0700

    Update progress

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -6,27 +6,25 @@
 @endsection
 
 @section('container')
-    <div class="nest text-center">
-        <div class="row">
-            <div class="col-5 align-items-center">
-                <div class="nested">
-                    <div class="test align-self-center">
-                        TEST
-                    </div>
-                </div>
-            </div>
-            <div class="col-1">
-                TEST
-            </div>
-            <div class="col">
-                <div class="col">
-                    TEST2
-                </div> 
-                <div class="col">
-                    TEST3
-                </div>
-            </div>
-
+    <div class="row">
+        <div class="col-md-4 align-items-center">
+            <img src="" alt="NOT FOUND" class="card-img-top">
+            <class class="card-body">
+                <h5 class="card-title">INSERT NAME</h5>
+                <h6 class="card-text">Total Steps: </h6>
+            </class>
         </div>
+        <div class="col-1">
+            TEST
+        </div>
+        <div class="col">
+            <div class="col">
+                TEST2
+            </div> 
+            <div class="col">
+                TEST3
+            </div>
+        </div>
+
     </div>
 @endsection
