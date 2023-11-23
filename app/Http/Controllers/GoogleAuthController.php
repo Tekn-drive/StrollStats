e323620ec96703b84d1123abb6c762aa25e2179c
DH3155 <darian.elbert@gmail.com>
2023-11-23 16:41:59 +0700

    Fixed Google Login

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class GoogleAuthController extends Controller
 {
