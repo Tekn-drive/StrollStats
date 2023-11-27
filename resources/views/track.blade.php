f9aa19bd66567efbba9eed5860a2ad3400a1f1d1
Nathan Setiawan <natsetiawan03@gmail.com>
2023-11-27 22:43:55 +0700

    improved track steps page

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -5,16 +5,10 @@
 @endsection
 
 @section('container')
-<div>
-
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-
+<div class="mt-4 mb-4 p-3 text-center">
     <h1>Add New Record!</h1>
     <h6>How many steps have you taken today?</h6>
+
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul class="mb-0">
@@ -25,55 +19,53 @@
         </div>
     @endif
 
-    <form action="{{ url('/processForm') }}" method="POST">
+    <form action="{{ url('/processForm') }}" method="POST" class="mt-4 mb-4">
         @csrf
         <div class="form-group">
-            <label for="steps">Enter the steps you taken:</label>
-            <input type="text" class="form-control" id="steps" name="steps" value="{{ old('steps') }}">
-            {{-- CRUD --}}
+            <label for="steps" class="form-label">Enter the steps you taken:</label>
+            <input type="text" class="form-control d-block mx-auto" id="steps" name="steps" value="{{ old('steps') }}" style="max-width: 400px;"> <!-- Constrain the width of input -->
             @error('steps')
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
-
-        <button type="submit" class="btn btn-primary mb-2">Register</button>
-
+        <button type="submit" class="btn btn-primary mt-3 mx-auto">Register</button>
     </form>
 </div>
-<div>
-    <h1>Track Your Steps!</h1>
-    <div>
-        <table class="table table-light">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Date recorded</th>
-                    <th scope="col">Steps Taken</th>
-                </tr>
-            </thead>
-            <tbody>
-            @php
-                $i = 0;
-            @endphp
-            @foreach($playerSteps as $step)
-                <tr>
-                    <th scope="row">{{ $i+1 }}</th>  
-                    <td>{{ $step->created_at }}</td> 
-                    <td>{{ $step->steps }}</td>
-                    <td>
-                        <form action="{{ route('steps.destroy', $step->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @php
-                    $i = $i + 1;
-                @endphp
-            @endforeach
-            </tbody>
-        </table>
+
+
+<div class="mt-4 p-3">
+    <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-8">
+            <h1 class="text-center mb-4">Track Your Steps!</h1>
+            <div class="table-responsive">
+                <table class="table table-hover table-striped">
+                    <thead class="thead-dark">
+                        <tr class="text-center">
+                            <th scope="col">#</th>
+                            <th scope="col">Date recorded</th>
+                            <th scope="col">Steps Taken</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($playerSteps as $index => $step)
+                        <tr class="text-center">
+                            <th scope="row">{{ $index + 1 }}</th>  
+                            <td>{{ $step->created_at }}</td> 
+                            <td>{{ $step->steps }}</td>
+                            <td>
+                                <form action="{{ route('steps.destroy', $step->id) }}" method="POST" class="d-flex justify-content-center">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
